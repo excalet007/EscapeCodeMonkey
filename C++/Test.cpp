d53f2f382f11864d2d475936a94ced48ba7a2ba0
excalet007 <excalet007@gmail.com>
2018-07-21 23:22:17 +0900

    no message

--- C++/Test.cpp
+++ C++/Test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
 
 int main()
 {
-	cout << "Hello World!";
+	vector<int> v = {5,7,9,4,6,8};
+	for(int x:v)
+		cout << x << endl;
+	
 	return 0;
 }
